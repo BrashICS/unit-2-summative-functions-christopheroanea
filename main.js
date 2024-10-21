a33c5ae5e20ce706edc8ce827ff867c032c800be
chris <christopher.oanea@stu.ocsb.ca>
2024-10-21 11:15:33 -0400

    thing thing at home summarative.html

--- main.js
+++ main.js
@@ -14,6 +14,23 @@ document.getElementById("vertex").addEventListener("click", vertex);
 
 /*** Functions ***/
 
+function delta(a, b){
+    let answer= (a-b)
+    return answer
+}
+
+function slope(x1, y1, x2, y2){
+    let answer = (y2 - y1) / (x2 - x1)
+    return answer
+}
+
+function average(a, b){
+    let answer = (a + b) / 2
+    return answer
+}
+
+
+
 // Round to the nearest `decimals` number of decimals
 function round(value, decimals) {
     return Math.round(value * 10**decimals) / 10**decimals;
@@ -23,7 +40,8 @@ function round(value, decimals) {
 function round_user(value) {
     // Get the number of decimals from the "rounding" box
     let d = Number(document.getElementById("rounding").value)
-
+    let answer = Math.round(value} * ${d}/ 10
+    return answer
 }
 
 // Calculate the y-value of a parabola from standand form
