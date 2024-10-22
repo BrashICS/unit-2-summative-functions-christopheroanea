77527cc92ef845b4cbbb1a46bdc711df821d87ad
chris <christopher.oanea@stu.ocsb.ca>
2024-10-22 11:15:31 -0400

    thing thing.html

--- main.js
+++ main.js
@@ -11,11 +11,12 @@
 /*** Event Listeners ***/
 document.getElementById("zeros").addEventListener("click", zeros);
 document.getElementById("vertex").addEventListener("click", vertex);
+document.getElementById("rectangle_volume").addEventListener("click", rectangle_volume)
 
 /*** Functions ***/
 
 function delta(a, b){
-    let answer= (a-b)
+    let answer = (a-b)
     return answer
 }
 
@@ -29,6 +30,15 @@ function average(a, b){
     return answer
 }
 
+function length(x1, y1, x2, y2){
+    let answer_1 = (delta(x2, x1))**2
+    let answer_2 = (delta(y2, y1))**2
+    let answer_full_1 = Math.sqrt(answer_1)
+    let answer_full_2 = Math.sqrt(answer_2)
+    let x = `${answer_full_1}, ${answer_full_2}`
+    return x
+}
+
 
 
 // Round to the nearest `decimals` number of decimals
@@ -40,14 +50,24 @@ function round(value, decimals) {
 function round_user(value) {
     // Get the number of decimals from the "rounding" box
     let d = Number(document.getElementById("rounding").value)
-    let answer = Math.round(value} * ${d}/ 10
+    let answer = Math.round(value * 10**d) / 10**d;
     return answer
 }
 
 // Calculate the y-value of a parabola from standand form
 function y_quad(a, b, c, x) {
-
+    let thing = a*x**2 + b*x + c
+    return thing
 }
+
+function rectangle_volume(){
+    let a = Number(document.getElementById("width").value)
+    let b = Number(document.getElementById("height").value)
+    let c = Number(document.getElementById("length").value)
+    let answer = a * b * c
+    document.getElementById("answer_of_thing").textContent = `The area is ${answer}`
+}
+
 
 // Determine the zeros of a quadratic using user-inputs for a, b, and c
 function zeros() {
